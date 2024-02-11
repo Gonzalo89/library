e7057d27fa23271783c26daab52d63aca1155bc0
Gonzalo Rodriguez <gonzalorodriguez89@gmail.com>
2024-02-11 11:15:29 -0300

    Remove not needed files

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: Gonzalo <fruta@gmail.com>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-const babelConfig = require('@nextcloud/babel-config')
-
-module.exports = babelConfig
--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Gonzalo <fruta@gmail.com>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-require_once __DIR__ . '/../../../tests/bootstrap.php';
