76224670900b720a980747d78a67e5e22473f645
Gonzalo Rodriguez <gonzalorodriguez89@gmail.com>
2024-02-11 10:57:20 -0300

    revert initial commit

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import App from './views/App.vue'
-import Vue from 'vue'
-Vue.mixin({ methods: { t, n } })
-
-const VueApp = Vue.extend(App)
-new VueApp().$mount('#content')
--- templates/main.php
+++
@@ -1,3 +0,0 @@
-<?php
-$appId = OCA\NoteBook\AppInfo\Application::APP_ID;
-\OCP\Util::addScript($appId, $appId . '-main');
